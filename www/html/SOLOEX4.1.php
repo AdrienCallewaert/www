47052dc5e8670c3de5d526c00255ac9a1d3895e0
Adrien Callewaert <76485285+AdrienCallewaert@users.noreply.github.com>
2021-03-17 17:03:26 +0100

    solo siute

---
+++ www/html/SOLOEX4.1.php
@@ -0,0 +1,11 @@
+<?php
+
+
+for ($repetition = 0 ; $repetition < 10 ; $repetition++) 
+{
+    echo '<p>Je ne doit pas copier sur on voision.' .$repetition. ' fois.</p>';
+}
+
+?>
+
+
---
+++ www/html/SOLOEX4.php
@@ -0,0 +1,26 @@
+<?php
+
+//$repetition = 0;
+//
+//while ($repetition < 10) 
+//{
+//    echo '<p>Je ne doit pas copier sur on voision.</p>';
+//    $repetition++;
+//}
+
+$repetition = 0;
+
+while ($repetition < 10) 
+{
+    echo '<p>Je ne doit pas copier sur on voision.' .$repetition. ' fois.</p>';
+    $repetition++;
+}
+
+
+
+//  $repetition = $repetition + 1;
+// ou
+// $repetition++; renvoi une incrementation +1 jusqua ce que l'on atteigne la limite
+?>
+
+
