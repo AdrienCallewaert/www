ceb37afe0decc58139a3c3e924437f6088da39d4
Adrien Callewaert <76485285+AdrienCallewaert@users.noreply.github.com>
2021-03-18 14:56:48 +0100

    Create SOLOEX5.php

---
+++ www/html/SOLOEX5.php
@@ -0,0 +1,74 @@
+<?php
+
+
+// //affiche tableu entre par entre (faut cnonnaitre le nombre de)
+//$nom = array('mathieux', 'marie', 'robert');
+//$personne = array('nom' =>'nebra', 'prenom' => 'mathieu', 'age' => 90);
+//
+//for ($numero = 0; $numero < 3; $numero++)
+//{
+//  echo '<p>' .$nom [$numero]. '</p>';
+//}
+
+
+//affiche tableu entre par entre (pas limite au nombre de cas)
+$nom = array('mathieux', 'marie', 'robert');
+$personne = array('nom' =>'nebra', 'prenom' => 'mathieu', 'age' => 90);
+
+
+
+//foreach ($nom as $nom)
+//{
+//  echo '<p>' .$nom. '</p>';
+//}
+
+
+
+//foreach ($personne as $detail)
+//{
+//  echo '<p>' .$detail. '</p>';
+//}
+
+foreach ($personne as $libele => $detail)
+{
+  echo '<p>' .$libele. ' vaut ' .$detail. '</p>';
+}
+
+
+
+
+
+// $personne = array('nom' =>'nebra', 'prenom' => 'mathieu', 'age' => 90);
+// print_r ($personne)
+
+
+
+
+
+
+//$prenom = array('mathieux', 'marie', 'robert');
+//echo $prenom [1];
+
+
+//echo print_r($prenom);
+//echo print_r($prenom);
+//$prenom[o] = 'mathieux';
+//$prenom[1] = 'marie';
+//$prenom[2] = 'robert';
+
+
+
+
+
+
+//
+//ou
+// 
+//$prenom = array('mathieux', 'marie', 'robert');
+//
+?>
+
+
+
+
+
